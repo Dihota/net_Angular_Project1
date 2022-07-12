9f53cfe92e4facd5288663989ddc522bfaf4af96
Diego Tamayo <diegotamayoctma@gmail.com>
2022-07-12 17:10:50 -0500

    Se agrega componente de login, nav y footer...se esta trabajando en el diseño del login

--- Frontend_Angular/Frontend/src/app/app.module.ts
+++ Frontend_Angular/Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoguinComponent } from './Component/loguin/loguin.component';
+import { NavComponent } from './Component/nav/nav.component';
+import { FooterComponent } from './Component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoguinComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
