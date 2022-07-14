86226b37ac853aee858f780b518fbecf66df09c1
Diego Tamayo <diegotamayoctma@gmail.com>
2022-07-14 17:00:31 -0500

    Se crea el componente registro

--- Frontend_Angular/Frontend/src/app/app-routing.module.ts
+++ Frontend_Angular/Frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './Component/footer/footer.component';
 import { LoguinComponent } from './Component/loguin/loguin.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 const routes: Routes = [
   //Define la ruta de inicio de la pagina 
-  {path: '', component: LoguinComponent}
+  {path: '', component: LoguinComponent},
+  {path: 'registro' , component: RegistroComponent}
   
 ];
 
--- Frontend_Angular/Frontend/src/app/app.module.ts
+++ Frontend_Angular/Frontend/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LoguinComponent } from './Component/loguin/loguin.component';
 import { NavComponent } from './Component/nav/nav.component';
 import { FooterComponent } from './Component/footer/footer.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoguinComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    RegistroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
